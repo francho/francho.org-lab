00c2c655e961187c366f90d7ad0925afd8716a76
Francho <lab@francho.org>
2011-11-11 20:51:26 +0100

    Añadido el código del artículo http://francho.org/2009/04/13/java-calculadora-sencilla-en-modo-grafico

---
+++ 0045-CalculadoraJavaGrafica/src/org/francho/java/calculadora/Main.java
@@ -0,0 +1,39 @@
+/**
+ * By Francho Joven - http://francho.org/lab/
+ *
+ * This work is licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+ * or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *
+ */
+/*
+ * Ejercicio: Calculadora sencilla
+ * - Implementar la Interfaz de usuario
+ * - Implementar los métodos siguientes a través de los listeners adecuados:
+ * - Sumar
+ * - Restar
+ * - Multiplicar
+ * - Dividir
+ */
+
+package org.francho.java.calculadora;
+
+/**
+ * 
+ * @author: $Author: franchux $
+ * @version: $Rev: 70 $
+ * @date: $Date: 2009-04-13 13:06:00 +0200 (lun 13 de abr de 2009) $ $Id:
+ *        Main.java 70 2009-04-13 11:06:00Z franchux $
+ */
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		VentanaCalculadora calculadora = new VentanaCalculadora();
+		calculadora.setVisible(true);
+	}
+
+}
---
+++ 0045-CalculadoraJavaGrafica/src/org/francho/java/calculadora/VentanaCalculadora.java
@@ -0,0 +1,196 @@
+/**
+ * By Francho Joven - http://francho.org/lab/
+ *
+ * This work is licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+ * or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *
+ */
+package org.francho.java.calculadora;
+
+/*
+ * Interfaz para nuestra calculadora sencilla
+ */
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 
+ * @author: $Author: franchux $
+ * @version: $Rev: 75 $
+ * @date: $Date: 2009-04-14 00:21:18 +0200 (mar 14 de abr de 2009) $ $Id:
+ *        VentanaCalculadora.java 75 2009-04-13 22:21:18Z franchux $
+ */
+public class VentanaCalculadora extends JFrame {
+
+	/** numero tecleado */
+	JTextField pantalla;
+
+	/** guarda el resultado de la operacion anterior o el nœmero tecleado */
+	double resultado;
+
+	/** para guardar la operacion a realizar */
+	String operacion;
+
+	/** Los paneles donde colocaremos los botones */
+	JPanel panelNumeros, panelOperaciones;
+
+	/** Indica si estamos iniciando o no una operaci—n */
+	boolean nuevaOperacion = true;
+
+	/**
+	 * Constructor. Crea los botones y componentes de la calculadora
+	 */
+	public VentanaCalculadora() {
+		super();
+		setSize(250, 300);
+		setTitle("Calculadora Simple");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+
+		// Vamos a dibujar sobre el panel
+		JPanel panel = (JPanel) this.getContentPane();
+		panel.setLayout(new BorderLayout());
+
+		pantalla = new JTextField("0", 20);
+		pantalla.setBorder(new EmptyBorder(4, 4, 4, 4));
+		pantalla.setFont(new Font("Arial", Font.BOLD, 25));
+		pantalla.setHorizontalAlignment(JTextField.RIGHT);
+		pantalla.setEditable(false);
+		pantalla.setBackground(Color.WHITE);
+		panel.add("North", pantalla);
+
+		panelNumeros = new JPanel();
+		panelNumeros.setLayout(new GridLayout(4, 3));
+		panelNumeros.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		for (int n = 9; n >= 0; n--) {
+			nuevoBotonNumerico("" + n);
+		}
+
+		nuevoBotonNumerico(".");
+
+		panel.add("Center", panelNumeros);
+
+		panelOperaciones = new JPanel();
+		panelOperaciones.setLayout(new GridLayout(6, 1));
+		panelOperaciones.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		nuevoBotonOperacion("+");
+		nuevoBotonOperacion("-");
+		nuevoBotonOperacion("*");
+		nuevoBotonOperacion("/");
+		nuevoBotonOperacion("=");
+		nuevoBotonOperacion("CE");
+
+		panel.add("East", panelOperaciones);
+
+		validate();
+	}
+
+	/**
+	 * Crea un boton del teclado numŽrico y enlaza sus eventos con el listener
+	 * correspondiente
+	 * 
+	 * @param digito
+	 *            boton a crear
+	 */
+	private void nuevoBotonNumerico(String digito) {
+		JButton btn = new JButton();
+		btn.setText(digito);
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				numeroPulsado(btn.getText());
+			}
+		});
+
+		panelNumeros.add(btn);
+
+	}
+
+	/**
+	 * Crea un bot—n de operacion y lo enlaza con sus eventos.
+	 * 
+	 * @param operacion
+	 */
+	private void nuevoBotonOperacion(String operacion) {
+		JButton btn = new JButton(operacion);
+		btn.setForeground(Color.RED);
+
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				operacionPulsado(btn.getText());
+			}
+		});
+
+		panelOperaciones.add(btn);
+	}
+
+	/**
+	 * Gestiona las pulsaciones de teclas numŽricas
+	 * 
+	 * @param digito
+	 *            tecla pulsada
+	 */
+	private void numeroPulsado(String digito) {
+		if (pantalla.getText().equals("0") || nuevaOperacion) {
+			pantalla.setText(digito);
+		} else {
+			pantalla.setText(pantalla.getText() + digito);
+		}
+		nuevaOperacion = false;
+	}
+
+	/**
+	 * Gestiona el gestiona las pulsaciones de teclas de operaci—n
+	 * 
+	 * @param tecla
+	 */
+	private void operacionPulsado(String tecla) {
+		if (tecla.equals("=")) {
+			calcularResultado();
+		} else if (tecla.equals("CE")) {
+			resultado = 0;
+			pantalla.setText("");
+			nuevaOperacion = true;
+		} else {
+			operacion = tecla;
+			if ((resultado > 0) && !nuevaOperacion) {
+				calcularResultado();
+			} else {
+				resultado = new Double(pantalla.getText());
+			}
+		}
+
+		nuevaOperacion = true;
+	}
+
+	/**
+	 * Calcula el resultado y lo muestra por pantalla
+	 */
+	private void calcularResultado() {
+		if (operacion.equals("+")) {
+			resultado += new Double(pantalla.getText());
+		} else if (operacion.equals("-")) {
+			resultado -= new Double(pantalla.getText());
+		} else if (operacion.equals("/")) {
+			resultado /= new Double(pantalla.getText());
+		} else if (operacion.equals("*")) {
+			resultado *= new Double(pantalla.getText());
+		}
+
+		pantalla.setText("" + resultado);
+		operacion = "";
+	}
+}
